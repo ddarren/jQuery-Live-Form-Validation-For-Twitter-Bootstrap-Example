c8103f9ddb89fc1f69fa5c965f9eff1a60c2b02b
Darren Smith <ddarrensmith@gmail.com>
2012-05-18 19:29:54 -0500

    initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JQueryLiveFormValidationForTwitterBootstrapExample::Application.config.secret_token = 'c51fd660b9b6f3cecc694ba36e2e5f9c40adc7ddeafe4aa65ec433b7b45843e8e188b475ef852f1db7533d0a48a33c0c9500c0d98ad46dac8831e7197585444b'
